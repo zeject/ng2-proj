ab5d9e57c19c02146e06ff0b5ce352cafb41cfff
zeject <424049902@qq.com>
2016-08-04 10:08:21 +0800

    router change #

--- app/main.ts
+++ app/main.ts
@@ -6,12 +6,17 @@ import { InMemoryDataService }               from './bean/in-memory-data.service
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { HTTP_PROVIDERS } from '@angular/http'
 
+// Add these symbols to override the `LocationStrategy`
+import { LocationStrategy,
+         HashLocationStrategy } from '@angular/common';
+
 import { AppComponent } from './index/app.component';
 import { APP_ROUTER_PROVIDERS } from './route/app.routes'
 
 bootstrap(AppComponent, [
 	APP_ROUTER_PROVIDERS, 
 	HTTP_PROVIDERS,
+	{ provide: LocationStrategy, useClass: HashLocationStrategy },
 	{ provide: XHRBackend, useClass: InMemoryBackendService },
     { provide: SEED_DATA, useClass: InMemoryDataService }
 ]);
